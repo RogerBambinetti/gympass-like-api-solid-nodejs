39aefd2e9aa3495718ecf1484af8411ff5a432a9
Roger <rogerbambinetti@hotmail.com>
2024-12-30 22:09:34 -0300

    adding getuserprofile usecase

--- src/use-cases/get-user-profile.ts
+++ src/use-cases/get-user-profile.ts
@@ -1,32 +1,25 @@
 import { compare } from "bcryptjs";
-import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 import { UserRepository } from "@/repositories/user-repository";
 import { User } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
-interface AuthenticateUseCaseInput {
-    email: string;
-    password: string;
+interface GetUserProfileUseCaseInput {
+    id: string;
 }
 
-interface AuthenticateUseCaseOutput {
+interface GetUserProfileUseCaseOutput {
     user: User
 }
 
-export class AuthenticateUseCase {
+export class GetUserProfileUseCase {
 
     constructor(private userRepository: UserRepository) { }
 
-    async execute({ email, password }: AuthenticateUseCaseInput): Promise<AuthenticateUseCaseOutput> {
-        const user = await this.userRepository.findByEmail(email);
+    async execute({ id }: GetUserProfileUseCaseInput): Promise<GetUserProfileUseCaseOutput> {
+        const user = await this.userRepository.findById(id);
 
         if (!user) {
-            throw new InvalidCredentialsError();
-        }
-
-        const doesPasswordMatch = await compare(password, user.password_hash);
-
-        if (!doesPasswordMatch) {
-            throw new InvalidCredentialsError();
+            throw new ResourceNotFoundError();
         }
 
         return { user };
