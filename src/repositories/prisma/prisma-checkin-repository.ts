984dffcae3f4fd499c20b6c3f61485bdfd532fb3
Roger <rogerbambinetti@hotmail.com>
2025-01-07 23:25:01 -0300

    implementing prisma-chekin-repository

--- src/repositories/prisma/prisma-checkin-repository.ts
+++ src/repositories/prisma/prisma-checkin-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+import { Checkin, Prisma } from "@prisma/client";
 import { CheckinRepository } from "@/repositories/check-in-repository";
 
 export class PrismaCheckinRepository implements CheckinRepository {
@@ -10,5 +10,55 @@ export class PrismaCheckinRepository implements CheckinRepository {
         });
 
         return checkin;
+    }
+
+    async save(data: Checkin) {
+        const checkin = await prisma.checkin.create({
+            data
+        });
+
+        return checkin;
+    }
+
+    async findByUserIdOnDate(userId: string, date: Date) {
+
+        const checkin = await prisma.checkin.findFirst({
+            where: {
+                user_id: userId,
+                created_at: {
+                    equals: date
+                }
+            }
+        });
+
+        return checkin;
+    }
+
+    async findManyByUserId(userId: string) {
+        const checkins = await prisma.checkin.findMany({
+            where: {
+                user_id: userId
+            }
+        });
+
+        return checkins;
+    }
+
+    async countByUserId(userId: string) {
+        const count = await prisma.checkin.count({
+            where: {
+                user_id: userId
+            }
+        });
+
+        return count;
+    }
+
+    findById(id: string) {
+        return prisma.checkin.findUnique({
+            where: {
+                id
+            }
+        });
     }
 }
